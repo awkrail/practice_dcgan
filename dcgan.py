dba1ab11a86e052723befc372f952c579bd8934e
misogil0116 <lack_un@yahoo.co.jp>
2018-02-28 14:11:52 +0900

    change GAN into DCGAN model

--- dcgan.py
+++ dcgan.py
@@ -5,22 +5,24 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Reshape
 from keras.layers.core import Activation
+from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import UpSampling2D
 from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.layers.core import Flatten
+from keras.layers.core import Flatten, Dropout
 from keras.optimizers import SGD
 
 def generator_model():
     model = Sequential()
     model.add(Dense(input_dim=100, output_dim=1024))
-    model.add(Activation('tanh'))
+    model.add(Activation('relu'))
     model.add(Dense(128*7*7))
-    model.add(Activation('tanh'))
+    model.add(Activation('relu'))
     model.add(Reshape((7, 7, 128), input_shape=(128*7*7,)))
     model.add(UpSampling2D(size=(2, 2)))
     model.add(Conv2D(64, (5, 5), padding='same'))
-    model.add(Activation('tanh'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(UpSampling2D(size=(2, 2)))
     model.add(Conv2D(1, (5, 5), padding='same'))
     model.add(Activation('tanh'))
@@ -33,14 +35,18 @@ def discriminator_model():
                padding='same',
                input_shape=(28, 28, 1))
     )
-    model.add(Activation('tanh'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, (5, 5)))
-    model.add(Activation('tanh'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(LeakyReLU(0.2))
+    # model.add(Activation('tanh'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (5, 5), subsample=(2, 2))) # +
+    model.add(LeakyReLU(0.2)) # +
+    # model.add(Activation('tanh'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Activation('tanh'))
+    model.add(Dense(256)) # model.add(Dense(1024))
+    # model.add(Activation('tanh'))
+    model.add(LeakyReLU(0.2)) # +
+    model.add(Dropout(0.5)) # +
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
     return model # discriminatorは何を誤差として捉えているのか?
@@ -51,6 +57,20 @@ def generator_containing_discriminator(g, d):
     d.trainable = False
     model.add(d)
     return model
+
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width = int(math.sqrt(num))
+    height = int(math.ceil(float(num)/width))
+    shape = generated_images.shape[1:3]
+    image = np.zeros((height*shape[0], width*shape[1]),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index/width)
+        j = index % width
+        image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = \
+            img[:, :, 0]
+    return image
 
 def train(batch_size):
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -79,7 +99,6 @@ def train(batch_size):
             d_loss = d.train_on_batch(X, y)
             noise = np.random.uniform(-1, 1, (batch_size, 100))
             d.trainable = False
-
             # generatorは, 本当の画像
             g_loss = d_on_g.train_on_batch(noise, [1] * batch_size)
             print("batch {} d_loss {}".format(index, d_loss))
